f6b418406a520a1077b85fb7c13c25dcc6e40b93
phlinhng <phlinhng@outlook.com>
2021-06-19 23:31:10 +0800

    rm docker

--- xray-config.sh
+++ xray-config.sh
@@ -1,31 +1,7 @@
 #!/bin/sh
 
-ARGO_TUNNEL_NAME=$1
-ARGO_TUNNEL_TOKEN=$2
-XRAY_VLESS_UUID=$3
-XRAY_TLS_CERT=$4
-XRAY_TLS_KEY=$5
-XRAY_GRPC_SERVICENAME=$6
-XRAY_VERSION=$7
-
-# Install Argo Tunnel Client
-curl -fsSL https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.tgz -o cloudflared-stable-linux-amd64.tgz
-tar -xf cloudflared-stable-linux-amd64.tgz
-mv cloudflared /usr/bin/cloudflared && chmod +x /usr/bin/cloudflared
-
-# Copy Argo TLS Token
-mkdir -p /root/.cloudflared
-echo $ARGO_TUNNEL_TOKEN > /root/.cloudflared/cert.pem
-
-# Install Xray-core
-curl -fsSL https://github.com/XTLS/xray-core/releases/download/v${XRAY_VERSION}/Xray-linux-64.zip -o Xray-linux-64.zip
-unzip -qq -o Xray-linux-64.zip
-mv xray /usr/bin/xray && chmod +x /usr/bin/xray
-mv geo*.dat /usr/bin
-
-# Copy TLS cert and key
-echo $XRAY_TLS_CERT > /etc/xray.crt && chmod 644 /etc/xray.crt
-echo $XRAY_TLS_KEY > /etc/xray.key && chmod 644 /etc/xray.key
+XRAY_VLESS_UUID=$1
+XRAY_GRPC_SERVICENAME=$2
 
 cat > "/xray-config.json" <<-EOF
 {
@@ -97,6 +73,3 @@ cat > "/xray-config.json" <<-EOF
   }
 }
 EOF
-
-/usr/bin/cloudflared tunnel run --url localhost:443 $ARGO_TUNNEL_NAME
-#xray run -config /xray-config.json
